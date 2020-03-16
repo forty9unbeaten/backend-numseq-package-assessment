63308b6613445035258af13d2abab0be5dc424fe
Rob Spears <rob.sp3ars@gmail.com>
2020-03-16 11:21:12 -0400

    Implement Sieve of Eratosthenes algorithm to generate primes

--- numseq/prime.py
+++ numseq/prime.py
@@ -1,6 +1,20 @@
 def primes(n):
     '''Returns a list of all prime numbers less than n'''
-    pass
+    # ALOT of reading to understand Sieve of Eratosthenes algorithm...WOW
+
+    if n > 2:
+        prime_nums = [True] * n
+        multiplier = 2
+        stopper = int(n ** .5)
+        while multiplier <= stopper:
+            if prime_nums[multiplier]:
+                for i in range(multiplier * multiplier, n, multiplier):
+                    if prime_nums[i]:
+                        prime_nums[i] = False
+            multiplier += 1
+        return [i for i in range(2, n) if prime_nums[i]]
+    else:
+        return []
 
 
 def is_prime(m):
